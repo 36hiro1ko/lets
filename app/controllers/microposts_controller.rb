de52338316c92bf48c1aa24a99cec15341e7b186
aco <example.com>
2014-10-16 07:57:38 +0900

    edited view

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   #before_action :signed_in_user, only: [:create, :destroy]
   before_action :signed_in_user
+  before_action :correct_user,   only: :destroy
   
   def index  
   end
@@ -11,7 +12,9 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to users_path
     else
-      render 'users/index'
+      @feed_items = []
+      #render 'users/index'
+      redirect_to users_path
     end
   end
 
@@ -22,5 +25,10 @@ class MicropostsController < ApplicationController
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
